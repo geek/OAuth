2bc1810c4a1009e8d629973d9eafaac3934b8af7
Wyatt Preul <wpreul@gmail.com>
2012-04-16 21:27:44 -0500

    Adding some error messages

---
+++ server/errors.js
@@ -0,0 +1,6 @@
+var ERRORS = {
+	invalid_request: "The request is missing a required parameter, includes an invalid parameter value, or is otherwise malformed.",
+    unauthorized_client: "The client is not authorized to request an authorization code using this method.",
+    access_denied: "The resource owner or authorization server denied the request.",
+    unsupported_response_type: "The authorization server does not support obtaining an authorization code using this method."
+};
