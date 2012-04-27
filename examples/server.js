58752eafcd72fd3e81486fad8f5bb793c6bed681
Wyatt Preul <wpreul@gmail.com>
2012-04-26 20:42:43 -0500

    Fixing a lot of issues, things mostly work now for authorizing a request

---
+++ examples/server.js
@@ -0,0 +1,36 @@
+var oauth = require('../server/oauthService.js'),
+	connect = require('connect'),
+	util = require('util');
+
+var client = {
+		id: '1',
+		secret: 'what',
+		isValidRedirectUri: function(uri) { return true; }
+	},
+	clientService = {
+		getById: function(id) {
+			return client;
+		}
+	},
+	tokenService = {
+		generateToken: function() { return 'blah'; }
+	},
+	authorizationService = {
+		saveAuthorizationCode: function(code) {
+
+		}
+	};
+
+var authorize = function(req, res) {
+		service = oauth.service(clientService, tokenService, authorizationService, 3600);
+		var oauthUri = service.authorizeRequest(req, '123');
+		res.write(util.inspect(oauthUri));
+		res.end();
+	};
+
+var server = connect()
+		.use(connect.query())
+		.use(connect.bodyParser())
+		.use('/oauth/authorize', authorize).listen(8001);
+
+console.log('listening on port 8001');
